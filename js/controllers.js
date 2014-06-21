69d4bd29aa874885025c29bc1fb4a0e745e6d5a0
Armando <flores@mtu.edu>
2014-06-20 23:38:26 -0700

    Styling on a Friday Night

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,24 @@
 'use strict';
 
 function TodoCtrl ($scope) {
-
-
    $scope.todos = [
-                  {text: 'Learn AngularJS', done: false}, 
-                  {text: 'Sleep Less', done: false},
-                  {text: 'Eat Less Peanut Butter', done: true}
-                  ];
+                  {text: 'Learn AngularJS', done: false},
+                  {text: 'Sleep Less', done: true},
+                  {text: 'Eat Less Peanut Butter', done: false}
+      ];
 
    $scope.getTotalTodos = function () {
          return $scope.todos.length;
    };
 
    $scope.clearCompleted = function () {
-         $scope.todos = _.filter($scope.todos, function (todo) {
-            return !todo.done;
-         })
-};
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return todo.done;
+        });
+    };
 
-   $scope.addTodo = function () {
-         $scope.todos.push({text:$scope.formTodoText, done:false});
-         $scope.formTodoText = '';
-   };
-
-
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+    };
 }
